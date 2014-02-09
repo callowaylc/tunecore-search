6347eff82b3497b15d937a608ccb163487333487
Ubuntu <ubuntu@ip-10-239-142-223.ec2.internal>
2014-02-09 00:24:01 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tunecore::Application.config.secret_key_base = '322685f871bd9f37183c0dc6cbab36d8dad169e6c1d2f5c4af9a380e6115f0f18407714d9972c9f44aad523164235e578ab63259813b258ba3e1241f7f540411'
