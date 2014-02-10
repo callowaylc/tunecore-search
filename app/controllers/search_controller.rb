f0e41884476443ad4be3aadd5dc2274d77a84c99
Ubuntu <ubuntu@ip-10-43-140-180.ec2.internal>
2014-02-10 04:41:20 +0000

    simple search appears to be working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,15 @@
+
 class SearchController < ApplicationController
   def index
 
-    params = filter
+    # retrieve search params if available
+    @page   = params[:page] || 1
+    @search = params.permit(
+      :artists, :album, :name
+    )
+    @search['results'] = search
 
+    # finally determine response type/content
     respond_to do |format|
       format.html 
       #format.json { render json: @test.errors, status: :unprocessable_entity }
@@ -12,7 +19,29 @@ class SearchController < ApplicationController
 
   private
 
-    def filter
-      params.permit(:q, :page, :limit)
+    def search
+      # run search against es cluster and return
+      # tire search instance 
+      page   = @page
+      params = @search.keep_if do | ignore, value |
+        value.present?
+      end
+      
+      # only perform query on available parameters
+      if params.length > 0
+        search = Tire.search 'tunecore' do
+          query do
+            params.each do | key, value |
+              string "#{key}:#{value}*"
+            end
+          end
+
+          #sort { by : }
+          
+          from (page - 1) * 100
+          size 100
+        end
+      end
     end
+
 end
